6a6b3ecc966769051428bc775e1af26fd98abe25
Rohan Virdi <vishalvirdi@Rohans-MacBook-Air.local>
2025-08-27 21:00:01 +0530

    for college

---
+++ college_lab_assignment1.c
@@ -0,0 +1,126 @@
+#include<stdio.h>
+int main(){
+// the area of square code with integer
+// int side; 
+// printf("enter the area ");
+// scanf("%d", &side);
+// printf("the area of square is : %d", side * side );
+
+// the area of circle code
+// float radius;
+// printf( "enter the radius of the circle");
+// scanf("%f", &radius);
+// printf("the radius of circle is : %f", 3.14 * radius * radius );
+
+// Q1. Write a program to calculate perimeter of rectangle.
+// Take sides, a & b, from the user. 
+
+
+// float l , w ;
+// printf("enter the length and breath ");
+// scanf("%f %f",&l , &w);
+// printf("calculate the perimeter of rectangle :  %f\n ", 2 * (l + w ));
+// Q2. Take a number(n) from user & output its cube(n*n*n).
+
+
+// int number;
+// printf("enter the value of number ");
+// scanf("%d",&number);
+// printf("the value of cube is : %d", number * number * number);
+// int ram = 4*3/3*2;
+// printf(" the value of ram %d",ram);
+// Write a program to check if a number is divisible by 2 or not.
+// int num;
+// printf("enter the number");
+// scanf("%d", &num);
+// printf(" %d", num % 2 ==0);
+
+// if else condition statements
+
+
+// int age;
+// printf("enter the age ");
+// scanf("%d", &age);
+// if (age>=18){
+// printf("the person is adult \n");
+// printf("it will get right to vote \n");
+// printf("the person will get their rights \n");
+// }
+// else {
+//     printf("the person is not adult \n");
+// }
+// printf("thanks you");
+// else if 
+// int num;
+// printf("enter the number less than 100 ");
+// scanf("%d",&num);
+// if (num >90 && num < 100){
+//     printf("You got A+ Grade");
+// }
+// else if (num>33 && num<60){
+//    printf("You are pass ");
+// }else if(num > 60 && num < 70){
+//     printf("You got C grade");
+// }else if(num > 70 && num <80){
+ 
+// printf("You got B grade");
+// }else if(num > 80 && num <90){
+//   printf("You got A grade");
+// }else{
+//    printf("Sorry You are fail");
+// }
+
+// i++, i=i+1,i+=1 
+// i-- , i=i-1, i-=1
+// ++i, --i IIV
+// int end;
+// int start;
+// printf("enter start and end ");
+// scanf("%d%d",&start,&end);
+// for(int i=start;i<=end;i++){
+//     printf("i= %d\n",i);
+// }
+
+
+// int i=10;
+// while(i>0)
+// {
+//     printf("i= %d\n",i);
+//     i--;
+
+// }
+// int j=0;
+// while(j<0)
+// {
+//     printf("j in while %d\n",j);
+//     j++;
+// }
+
+// int i=0;
+// // code will run atleast one time even if conditon is false - Do while loop
+// do
+// {
+//      printf("i in do while %d\n",i);
+//      i++;
+// } while (i<0);
+
+
+// for(int i=0;i<10;i++){
+//     if(i==4  || i==8){
+//         continue;
+//     }
+//     printf("i= %d\n",i);
+}
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
