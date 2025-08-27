17099006fd46ca348d94e6a556df865ea891166f
rohanvirdi <rohanvirdi007@gmail.com>
2025-08-27 21:03:42 +0530

    Add files via upload

---
+++ pratice_c_code.c
@@ -0,0 +1,48 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+// area of square
+/* 
+int side; 
+printf("enter the area ");
+scanf("%d", &side);
+printf("the area of square is : %d", side * side );
+*/
+// Online C compiler to run C program online
+
+// area of square in flot value 
+
+// float side; 
+// printf("enter the area ");
+// scanf("%f", &side);
+// printf("the area of square in float value  : %f", side * side );
+
+
+float radius;
+printf( "enter the radius of the circle");
+scanf("%f", &radius);
+printf("the radius of circle is : %f", 3.14 * radius * radius );
+
+// int number;
+// printf("enter the number ");
+// scanf("%d", &number );
+
+// if(number%2==0){
+//     printf("Your number is even:  %d",number);
+// }else{
+//     printf("Your number is odd:  %d",number);
+// }
+// int countEven=0;
+// for(int i=6;i>0;i--){
+//     printf("value of i : %d\n",i);
+//     if(i%2==0){
+//         countEven++;
+//         printf("I GOT MY FIRST EVEN NUMBER\n");
+//         break;
+//     }
+// }
+// printf("total even numbers are %d\n",countEven);
+
+
+   return 0;
+}
